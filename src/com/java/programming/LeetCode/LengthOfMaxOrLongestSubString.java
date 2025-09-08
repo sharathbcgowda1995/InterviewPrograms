6bd7aab36e2bda6f3ad4ba83f91478966185d69f
Sharath B C <sharathbc@Sharaths-MacBook-Air.local>
2025-09-08 22:40:41 +0530

    Code updated

--- src/com/java/programming/LeetCode/LengthOfMaxOrLongestSubString.java
+++ src/com/java/programming/LeetCode/LengthOfMaxOrLongestSubString.java
@@ -1,12 +1,11 @@
 package com.java.programming.LeetCode;
 
-import java.util.HashSet;
 import java.util.*;
 // https://docs.google.com/document/d/1LS1ap3uXQo3nMdd8LjTaQ0cSGXS_NpSbr1xkmQOO3jw/edit?tab=t.0
 // If element that we are checking is present then remove it from the list , update the start index
 // and if not present then add it to the list , add to the list + update the end index + calculate max_length
 
-public class LengthOfMaxSubString {
+public class LengthOfMaxOrLongestSubString {
 
     public static int findStringLength(String st) {
        int start =0;
@@ -14,22 +13,36 @@ public class LengthOfMaxSubString {
        int max_length =0 ;
 
         List<Character> list = new ArrayList<>();
+        String maxSubstring = "";
 
         while(end< st.length()){
             if(!list.contains(st.charAt(end))){
+
                 list.add(st.charAt(end));
                 end++;
-                max_length = Math.max(max_length,list.size());
+
+                // update max if needed
+                if (list.size() > max_length) {
+                    max_length = list.size();
+                    // build substring from current window
+                    StringBuilder sb = new StringBuilder();
+                    for (Character c : list){
+                        sb.append(c);
+                    }
+                    maxSubstring = sb.toString();
+                }
+
             }else{
                 list.remove(Character.valueOf(st.charAt(start)));
                 start++;
             }
         }
+        System.out.println("Maximum substring is : " + maxSubstring);
         return max_length;
     }
 
     public static void main(String[] args) {
-        String value = "abcdefgghhg"; // Test string
-        System.out.println(LengthOfMaxSubString.findStringLength(value)); // Output should be 6 for "abcdef"
+        String value = "abcabcbb"; // Test string
+        System.out.println(LengthOfMaxOrLongestSubString.findStringLength(value)); // Output should be 6 for "abcdef"
     }
 }
---
+++ src/com/java/programming/LeetCode/TargetSumUsingMap.java
@@ -0,0 +1,48 @@
+package com.java.programming.LeetCode;
+
+import java.util.HashMap;
+
+public class TargetSumUsingMap {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int targetSum = 5;
+        int[] resultArr = findSumIndexUsingMap(arr, targetSum);
+
+        if (resultArr != null) {
+            System.out.println("Indexes are : " + resultArr[0] + " " + resultArr[1]);
+        } else {
+            System.out.println("Match not found");
+        }
+    }
+
+    public static int[] findSumIndexUsingMap(int[] arr, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] resultArrayPositions = new int[2];
+
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            int value = target - arr[i];
+
+            if (map.containsKey(value)) {
+
+                resultArrayPositions[0] = i;
+                resultArrayPositions[1] = map.get(value);
+                return resultArrayPositions;
+
+            } else {
+
+                map.put(arr[i], i);
+            }
+
+        }
+        return null;
+    }
+
+}
+/*
+Time Complexity = O(n)
+Space Complexity = O(n)
+ */
